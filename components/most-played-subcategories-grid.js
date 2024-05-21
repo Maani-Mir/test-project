7961928ee1c15d5a63b370d5466ef7c8641042d0
Salman Arif <salmanarifmir2000@gmail.com>
2024-05-21 12:06:37 +0500

    max width utility edit in subcategories

--- components/most-played-subcategories-grid.js
+++ components/most-played-subcategories-grid.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SubCategoriesGrid() {
   return (
     <div className="rounded-b-lg overflow-hidden">
-      <div className="text-nowrap  bg-[#1D194A] px-20 py-3">
+      <div className="text-nowrap max-w-[100%] bg-[#1D194A] px-20 py-3">
         Responsibilities in the house
       </div>
       <div className="text-nowrap max-w-[100%] bg-[#1B1743] px-20 py-3">
